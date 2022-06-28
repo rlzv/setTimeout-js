4ab72447a26bbe0106a830a19a3a85b60782bf6a
Lazarov <robert.lazarov02@e-uvt.ro>
2022-06-28 12:32:23 +0300

    setting timeouts

---
+++ app.js
@@ -0,0 +1,19 @@
+
+// console.log("HELLLOOO!....")
+// setTimeout(() => {
+//     console.log("are you still there?!")
+// }, 3000) // printeaza dupa 3 secunde mesajul
+
+
+// console.log("Goodbye!")
+
+
+
+const id = setInterval(() => {
+    console.log(Math.random())
+
+}, 2000);
+
+//la un interval de 2s se genereaza un nr random
+
+//clearInterval(id) sa oprim generarea continua
